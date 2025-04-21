7644bc2507255c7e9424eebb5a12445e7b302aed
Vyshnavi21504 <Vyshnavi21504@gmail.com>
2025-04-21 16:32:22 +0530

    commit changes

--- Week3/ques3.jsx
+++
@@ -1,8 +0,0 @@
-function UserCard({ name, email }) {
-    return (
-      <div className="max-w-sm bg-white shadow-lg rounded-lg p-4 m-4 border border-gray-200">
-        <h2 className="text-xl font-bold text-gray-800">{name}</h2>
-        <p className="text-gray-600">{email}</p>
-      </div>
-    );
-  }
